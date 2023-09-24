943fdbecf09506daaf3aec62df33a7e137cc8540
Abir <hasanuzzaman_abir@outlook.com>
2023-09-24 19:38:52 +0600

    Added Lab 1 form and table

---
+++ Lab_task_1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Form</title>
+</head>
+
+<body>
+    <fieldset>
+        <div style="text-align: center;">
+            <!-- Heading -->
+            <h1>Login Page</h1>
+            <!-- Name -->
+            User Name: <input type="text"> <br>
+            <hr>
+            <!-- Password -->
+            Password : <input type="password"> <br>
+            <hr>
+            <!-- Button -->
+            <button>Submit</button>
+        </div>
+    </fieldset>
+
+    <br><br>
+
+    <h1>Course</h1>
+    <table border="1">
+        <!-- Table Header -->
+        <tr>
+            <th>ID</th>
+            <th>ID</th>
+            <th>Course</th>
+        </tr>
+        <!-- 1st Row -->
+        <tr>
+            <td>17</td>
+            <td>Ab</td>
+            <td rowspan="2">Web</td>
+        </tr>
+        <!-- 2nd Row -->
+        <tr>
+            <td>19</td>
+            <td rowspan="2">Cb</td>
+        </tr>
+        <!-- 3rd Row -->
+        <tr>
+            <td>19</td>
+            <td>CN</td>
+        </tr>
+    </table>
+</body>
+
+</html>
